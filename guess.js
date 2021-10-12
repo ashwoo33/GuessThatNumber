b8d537e9e83d144218fd186cc429438bf0b77e3a
Ashley Wooten <ashley.cummings.33.ac@gmail.com>
2021-10-12 10:14:52 -0500

    Created guess.js file. Also, created starting variables for project.

---
+++ guess.js
@@ -0,0 +1,9 @@
+// GUESS THAT NUMBER
+// Message to be used throughout the file/project
+const enterNumText = `Please enter a number greater than zero`;
+
+// For restarting the game
+let restartGame = true;
+
+// 
+let rangeNum;
